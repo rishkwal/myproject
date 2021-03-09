6812bba971b3592b6ba2bc46ba119ba2fd48f973
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-03-09 12:42:49 +0530

    before state

--- App.tsx
+++ App.tsx
@@ -5,8 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is called typescript</Text>
+      <View style={styles.header}>
+      <Text style={styles.boldText}>This is called typescript</Text>
       <StatusBar style="auto" />
+      </View>
+      <View style={styles.body}>
+          <Text style={styles.boldText}>This is called typescript!!!!</Text>
+          <Text style={styles.boldText}>This is called typescript!!!!</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +24,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  boldText:
+  {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }
 });
